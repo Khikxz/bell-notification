2dd989a4237adc15c15b7bfe80ed318d932cebf0
Khikxz <khikxz.corbix13542@gmail.com>
2024-11-15 09:06:27 +0800

    added bell animation

--- script.js
+++ script.js
@@ -6,4 +6,9 @@ button.addEventListener("click", () => {
     const counter = Number(notify.getAttribute("data-count")) || 0;
     notify.setAttribute("data-count", counter + 1);
     notify.classList.add("count");
+    notify.classList.add("notify");
 });
+
+notify.addEventListener("animationend", ()=> {
+    notify.classList.remove("notify");    
+})
